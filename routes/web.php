47e2229efc4e798c9b2047d4469b070d957a33b9
NOOHH <vmdelavega03@gmail.com>
2025-06-27 01:04:34 +0800

    FULL ENROLLMENT FORM

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('homepage'); // Make sure this matches your view name
@@ -10,7 +11,18 @@ Route::get('/enrollment', function () {
     return view('enrollment'); // Make sure this matches your view name
 });
 
+
+Route::get('/enrollment/full', function () {
+    return view('registration.Full_enrollment'); // ✅ use dot notation
+})->name('enrollment.full');
+
+
 Route::post('/register', function (){
     return 'Successfully login';
 });
 
+Route::get('/student/register', function () {
+    return view('student-register');
+});
+
+Route::post('/student/register', [StudentController::class, 'store'])->name('student.register');
