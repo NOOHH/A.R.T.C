484d398138317df0688ac220ee63b7571f39289b
NOOHH <vmdelavega03@gmail.com>
2025-08-03 12:42:38 +0800

    Fixes

--- resources/views/admin/admin-modules/admin-modules.blade.php
+++ resources/views/admin/admin-modules/admin-modules.blade.php
@@ -3,7 +3,6 @@
 @section('title', 'Modules')
 
 @push('styles')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css" rel="stylesheet">
 <link href="{{ asset('css/admin/admin-modules.css') }}" rel="stylesheet">
 @endpush
--- resources/views/admin/students/index.blade.php
+++ resources/views/admin/students/index.blade.php
@@ -1,8 +1,6 @@
 @extends('admin.admin-dashboard-layout')
 
 @push('styles')
-  {{-- Bootstrap CSS for pagination, forms, table, modal, etc. --}}
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   {{-- Bootstrap Icons --}}
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
 @endpush
